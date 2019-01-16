9f799563f901e30bfbd39421ad208616b8f8d5b0
Vincent Blouin <vincent.blouin@gmail.com>
2019-01-16 18:03:26 -0500

    financement

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -220,6 +220,23 @@
                 </p>
             </div>
         </Cercle>
+        <Cercle title="Financement" anchor="financement" class="pa-0" image="financement.jpg">
+            <!--https://pixabay.com/fr/le-crowdfunding-financement-id%C3%A9es-3576868/-->
+            <div slot="subtitle">
+                La créativité, la débrouille et la persévérence sont au rendez-vous pour ceux qui veulent développer leur sens de l'entreprenariat social.
+            </div>
+            <div slot="content">
+                <p>
+                    Et continuer à rendre la merveilleuse expérience du Loco Local possible pour plus longtemps encore.
+                </p>
+                <p>
+                    La location de salle, la vente de produits nettoyants, le chocolat, les serviettes sanitaires etc, le groupe d'achat, le reconditionnement d'ordinateur sont tous des moyens actuels de payer le local.
+                </p>
+                <p>
+                    Venez donc y ajouter votre touche personnelle.
+                </p>
+            </div>
+        </Cercle>
     </div>
 </template>
 
