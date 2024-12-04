3d3dceb1f1494565674bb759adc8bd881acc620c
Vincent Blouin <vincent.blouin@gmail.com>
2024-12-04 16:51:29 -0500

    slogan and mobile design

--- src/components/CercleWrap.vue
+++ src/components/CercleWrap.vue
@@ -1,12 +1,16 @@
 <template>
-  <v-row class="vh-center font-weight-thin pr-6" :id="anchor">
+  <v-row class="vh-center font-weight-thin" :id="anchor"
+         :class="{
+            'pr-6': $vuetify.display.mdAndUp
+         }"
+  >
     <v-col cols="12">
       <v-card flat :class="{
                 'mt-5 mb-12': $vuetify.display.mdAndUp,
                 'pl-4 mt-5 mb-4': $route.name === 'home' && $vuetify.display.smAndDown
             }">
         <v-row :reverse="imageAtRight" fill-height>
-          <v-col cols="12" md="5">
+          <v-col cols="12" md="5" class="pa-0">
             <v-carousel v-if="image && isCarousel" width="100%">
               <v-carousel-item
                   v-for="(item,i) in image"
@@ -27,17 +31,28 @@
             <slot name="image"></slot>
           </v-col>
           <v-col cols="12" md="7" left>
-            <v-card-title primary-title class="mt-0 pt-0 text-h5 text-left font-weight-bold" :class="{
-                            'mt-4' : $vuetify.display.smAndDown
+            <v-card-title primary-title class="mt-0 pt-0 text-h5 text-left font-weight-bold"
+                          :class="{
+                            'mt-4 pa-0' : $vuetify.display.smAndDown
                         }">
               {{ title }}
               <slot name="title"></slot>
             </v-card-title>
-            <div class="text-smaller font-weight-bold text-left mt-1 ml-4 text-grey-darken-2">
+            <div class="text-smaller font-weight-bold text-left mt-1 text-grey-darken-2"
+                 :class="{
+                    'ml-4' : $vuetify.display.mdAndUp,
+                    'mt-4 pa-0' : $vuetify.display.smAndDown
+                 }"
+            >
               <slot name="subtitle"></slot>
             </div>
-            <v-card-text class="text-justify text pr-8 text-h6 font-weight-regular cercle-content"
-                         style="line-height:1.8">
+            <v-card-text class="text-justify text text-h6 font-weight-regular cercle-content"
+                         style="line-height:1.8"
+                         :class="{
+                           'pr-8' :$vuetify.display.mdAndUp,
+                           'pa-0 pr-4' :$vuetify.display.smAndDown
+                         }"
+            >
               <slot name="content"></slot>
             </v-card-text>
           </v-col>
