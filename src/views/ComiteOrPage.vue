e5e66b7697168883905bc70fc4f375cde3b21322
Vincent Blouin <vincent.blouin@gmail.com>
2024-12-18 12:06:10 -0500

    404 page

--- src/views/ComiteOrPage.vue
+++ src/views/ComiteOrPage.vue
@@ -1,7 +1,7 @@
 <template>
   <PageWrap>
     <v-overlay
-        v-if="comite === null && page === null"
+        v-if="comite === null && page === null && !isPageNotFound"
         :model-value="true"
         class="align-center justify-center"
         absolute
@@ -18,7 +18,7 @@
             Ce comité est archivé.
           </template>
           <div class="text-black text-left">
-             Les informations dans cette page ne sont probablement plus valables.
+            Les informations dans cette page ne sont probablement plus valables.
           </div>
         </v-alert>
       </v-col>
@@ -33,6 +33,13 @@
       <v-col cols="0" md="1" lg="2">
       </v-col>
     </v-row>
+    <v-empty-state
+        v-if="isPageNotFound"
+        headline="Oops 404"
+        title="Page introuvable"
+        text="La page que vous chercher ne semble pas exister."
+        :image="require('@/assets/FB logo.jpg')"
+    ></v-empty-state>
   </PageWrap>
 </template>
 
@@ -49,6 +56,8 @@ const route = useRoute();
 const comite = ref(null)
 
 const page = ref(null);
+
+const isPageNotFound = ref(false)
 
 async function buildPage() {
   const response = await WordpressService.api().get(
@@ -72,6 +81,7 @@ watch(route, mathComiteWithSlug);
 async function mathComiteWithSlug() {
   comite.value = null;
   page.value = null;
+  isPageNotFound.value = false;
   if (comitesStore.$state.list === null) {
     return;
   }
@@ -81,10 +91,10 @@ async function mathComiteWithSlug() {
   if (await buildPage()) {
     return;
   }
-  const response = await WordpressService.api().get(
-      "comite_page_archive?_embed&slug=" + route.params.slug
-  );
-  comite.value = response.data[0]
+  if (await buildComiteArchive()) {
+    return;
+  }
+  isPageNotFound.value = true;
 }
 
 async function buildComite() {
@@ -93,6 +103,17 @@ async function buildComite() {
   })
   if (comitesHavingSlug.length) {
     comite.value = comitesHavingSlug[0]
+    return true;
+  }
+  return false;
+}
+
+async function buildComiteArchive() {
+  const response = await WordpressService.api().get(
+      "comite_page_archive?_embed&slug=" + route.params.slug
+  );
+  if (response.data.length) {
+    comite.value = response.data[0]
     return true;
   }
   return false;
